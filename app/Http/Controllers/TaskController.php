36ac547a2492bc01243f26085f98f6082b61226b
BakhadyrovF <BakhadyrovF@gmail.com>
2022-02-04 23:39:27 +0500

    first

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TaskRequest;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        if(isset($_GET["search"]) || isset($_GET["completed"]))
+        {
+            return $this->search();
+        }
+
+        return response($tasks, 200);
+
+    }
+
+
+    public function store(TaskRequest $request)
+    {
+        $data = $request->validated();
+
+        $task = Task::create($data);
+
+        return response($task, 201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+
+        return response($task, 200);
+    }
+
+
+    public function update(TaskRequest $request, $id)
+    {
+        $data = $request->validated();
+        $task = Task::findOrFail($id);
+
+        $task->update($data);
+
+        return response($task, 202);
+
+
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+
+        if(!$task)
+        {
+            return response([
+                "message" => "Task with id:$id not exists"
+            ], 403);
+        }
+        $task->delete($id);
+
+        return response(1, 200);
+
+    }
+
+    public function search()
+    {
+
+
+        if(isset($_GET["search"]) && isset($_GET["completed"]))
+        {
+            $tasks = Task::where("title", "LIKE", "%".$_GET["search"]."%")->where("completed", $_GET["completed"])->get();
+
+            return response($tasks, 200);
+        }
+
+        if(isset($_GET["search"]))
+        {
+            $tasks = Task::where("title", "LIKE", "%".$_GET["search"]."%")->get();
+
+            return response($tasks, 200);
+        }
+
+        if(isset($_GET["completed"]))
+        {
+            $tasks = Task::where("completed", $_GET["completed"])->get();
+
+            return response($tasks, 200);
+        }
+    }
+}
---
+++ database/migrations/2022_02_04_165748_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("content");
+            $table->smallInteger("completed", false, true);
+            $table->smallInteger("parent_id", false, true)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
